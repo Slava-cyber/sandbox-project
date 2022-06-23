7436b563795af81dd236b2069b3473fb32eaf280
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-06-23 21:35:01 +0700

    T2-5 add controller for main page

---
+++ src/Controllers/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\System\Controller;
+use App\System\View as View;
+use App\Models\Users\User as User;
+
+class MainController extends Controller
+{
+    public function actionIndex()
+    {
+        $this->view->render('Main/main');
+        return true;
+    }
+
+}
