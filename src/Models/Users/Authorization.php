132072449f9a3623ba5c1ae9f66e436913818af6
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-06-28 13:34:12 +0700

    change coockie lifetime

--- src/Models/Users/Authorization.php
+++ src/Models/Users/Authorization.php
@@ -6,7 +6,7 @@ namespace App\Models\Users;
 class Authorization {
     public static function createToken(User $user): void {
         $token = $user->getId() . ':' . $user->getAuthToken();
-        setcookie('token', $token, time()+3600, '/', '', false, true);
+        setcookie('token', $token, time()+36000, '/', '', false, true);
     }
 
     public static function getUserByToken() : ?User {
