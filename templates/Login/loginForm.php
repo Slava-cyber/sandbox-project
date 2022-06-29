21508ac2fd833b2cd2abcdf964024fef2f928194
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-06-29 15:59:30 +0700

    T2-templates-fix bug related to viewing content on mobile phones

--- templates/Login/loginForm.php
+++ templates/Login/loginForm.php
@@ -3,7 +3,7 @@
 <head>
     <metacharset="utf-8">
     <title>Авторизация</title>
-    <meta bane="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
 </head>
--- templates/Main/main.php
+++ templates/Main/main.php
@@ -3,7 +3,7 @@
 <head>
     <metacharset="utf-8">
     <title>Главная</title>
-    <meta bane="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
 </head>
@@ -47,7 +47,7 @@
 
     <div class="container mt-5 mb-5">
         <div class="row justify-content-center">
-            <div class="col-md-10 bg-white p-3">
+            <div class="col-md-10 col-sm-12 bg-white p-3">
                 <div class="row">
                     <form name="search" method="POST" action="" id="search">
                         <div class="row">
@@ -65,18 +65,18 @@
                                 <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
                                     <div class="accordion-body">
                                         <div class="row">
-                                            <div class="col-md-5">
+                                            <div class="col-md-5 col-sm-12">
                                                 <div class="form-group py-2">
                                                     <input type="text" name="town" placeholder="Город" class="form-control">
                                                 </div>
                                             </div>
-                                            <div class="col-md-2">
+                                            <div class="col-md-3 col-sm-12">
                                                 <div class="form-group py-2">
                                                     <input type="date" name="date" placeholder="Дата" class="form-control">
                                                     <small id="dateHelp" class="form-text form-muted">Укажите дату</small>
                                                 </div>
                                             </div>
-                                            <div class="col-md-2">
+                                            <div class="col-md-2 col-sm-12">
                                                 <div class="form-group py-2">
                                                     <input type="time" name="time" class="form-control">
                                                     <small id="timeHelp" class="form-text form-muted">Укажите время</small>
@@ -84,7 +84,7 @@
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-md-5">
+                                            <div class="col-md-5 col-sm-12">
                                                 <div class="form-group py-2">
                                                     <select class="form-select form-control">
                                                         <option selected>Выберите категорию</option>
@@ -94,9 +94,9 @@
                                                     </select>
                                                 </div>
                                             </div>
-                                            <div class="col-md-5">
+                                            <div class="col-md-5 col-sm-12">
                                                 <div class="form-group py-2">
-                                                    <select class="form-select form-control">
+                                                    <select class="form-select form-control" disabled>
                                                         <option selected>Выберите подкатегорию</option>
                                                         <option value="1">One</option>
                                                         <option value="2">Two</option>
@@ -116,6 +116,182 @@
                         </div>
                     </form>
                 </div>
+                <hr class="separator mt-1">
+                <div class="d-flex flex-row">
+                    <div class="d-sm-inline-flex col-md-12">
+                        Сортировать:
+                        <div class="me-3 ms-3">
+                            <a href="/profile/edit">Время</a>
+                        </div>
+                        <div class="me-3 ms-3">
+                            <a href="/profile/edit" clas="me-3 ms-3" >Рейтинг</a>
+                        </div>
+                    </div>
+                </div>
+                <hr class="separator mt-1">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3>Игра в теннис корт НГУ</h3>
+                    </div>
+                    <div class="d-sm-inline-flex col-md-12">
+                        <div class="me-3">
+                            Категория:
+                        </div>
+                        <div class="me-3">
+                            <strong>Спорт</strong>
+                        </div>
+                        <div class="me-3">
+                            Дата и время:
+                        </div>
+                        <div class="me-3">
+                            <strong>12.08.22
+                                12:00</strong>
+                        </div>
+                    </div>
+         <!--           <div class="d-sm-inline-flex col-md-12">
+                        <div class="me-3">
+                            Автор:
+                        </div>
+                        <div class="me-3">
+                            <a href="/profile/test7 class="me-3 ms-3">test7</a>
+                        </div>
+                        <div class="me-3">
+                            Рейтинг:
+                        </div>
+                        <div class="me-3">
+                            <strong>5.0</strong>
+                        </div>
+                        <div class="me-3">
+                            Город:
+                        </div>
+                        <div>
+                            <strong>Новосибирск</strong>
+                        </div>
+                    </div> -->
+                    <div class="col-md-12">
+                        <p>
+                            <a class="link-dark" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                Дополнительная информация
+                            </a>
+                        </p>
+                        <div class="collapse" id="collapseExample">
+                            <div class="d-sm-inline-flex col-md-12 mb-3">
+                                <div class="me-3">
+                                    Автор:
+                                </div>
+                                <div class="me-3">
+                                    <a href="/profile/test7">test7</a>
+                                </div>
+                                <div class="me-3">
+                                    Рейтинг:
+                                </div>
+                                <div class="me-3">
+                                    <strong>5.0</strong>
+                                </div>
+                                <div class="me-3">
+                                    Город:
+                                </div>
+                                <div>
+                                    <strong>Новосибирск</strong>
+                                </div>
+                            </div>
+                            <div class="com-md-12 card card-body">
+                                Описание Некоторый заполнитель для компонента сворачивания. Эта панель по умолчанию скрыта, но открывается, когда пользователь активирует соответствующий триггер.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <a href="#" role="button">Отправить запрос</a>
+                        <div class="col-md-12">
+                            <span class="text-primary">Статус запроса: Ожидает ответа</span>
+                        </div>
+                    </div>
+                    <hr class="separator mt-1">
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3>Виртуальная реальность Академгородок</h3>
+                    </div>
+                    <div class="d-sm-inline-flex col-md-12">
+                        <div class="me-3">
+                            Категория:
+                        </div>
+                        <div class="me-3">
+                            <strong>Квесты</strong>
+                        </div>
+                        <div class="me-3">
+                            Дата и время:
+                        </div>
+                        <div class="me-3">
+                            <strong>12.08.22
+                                12:00</strong>
+                        </div>
+                    </div>
+                    <!--           <div class="d-sm-inline-flex col-md-12">
+                                   <div class="me-3">
+                                       Автор:
+                                   </div>
+                                   <div class="me-3">
+                                       <a href="/profile/test7 class="me-3 ms-3">test7</a>
+                                   </div>
+                                   <div class="me-3">
+                                       Рейтинг:
+                                   </div>
+                                   <div class="me-3">
+                                       <strong>5.0</strong>
+                                   </div>
+                                   <div class="me-3">
+                                       Город:
+                                   </div>
+                                   <div>
+                                       <strong>Новосибирск</strong>
+                                   </div>
+                               </div> -->
+                    <div class="col-md-12">
+                        <p>
+                            <a class="link-dark" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                Дополнительная информация
+                            </a>
+                        </p>
+                        <div class="collapse" id="collapseExample">
+                            <div class="d-sm-inline-flex col-md-12 mb-3">
+                                <div class="me-3">
+                                    Автор:
+                                </div>
+                                <div class="me-3">
+                                    <a href="/profile/test7">test7</a>
+                                </div>
+                                <div class="me-3">
+                                    Рейтинг:
+                                </div>
+                                <div class="me-3">
+                                    <strong>5.0</strong>
+                                </div>
+                                <div class="me-3">
+                                    Город:
+                                </div>
+                                <div>
+                                    <strong>Новосибирск</strong>
+                                </div>
+                            </div>
+                            <div class="com-md-12 card card-body">
+                                Описание Некоторый заполнитель для компонента сворачивания. Эта панель по умолчанию скрыта, но открывается, когда пользователь активирует соответствующий триггер.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <a href="#" role="button">Отправить запрос</a>
+                        <div class="col-md-12">
+                            <span class="text-primary">Статус запроса: Ожидает ответа</span>
+                        </div>
+                    </div>
+                    <hr class="separator mt-1">
+                </div>
+
+
+
             </div>
         </div>
     </div>
