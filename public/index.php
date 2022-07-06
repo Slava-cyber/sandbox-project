f08e7975321007322452531c4a20bad7e80033b2
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-07-06 19:29:56 +0700

    T3 - 2 - index - fix error display mechanism

--- public/index.php
+++ public/index.php
@@ -1,14 +1,19 @@
 <?php
 define('ROOT', dirname(__FILE__) . '/../src/');
 
-$errorStatus = (require ROOT . '../settings/db.php')['error'];
+$config = require ROOT . '../settings/db.php';
+$errorStatus = false;
+if (isset($config['error'])) {
+    $errorStatus = $config['error'];
+}
+
 if ($errorStatus) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
 }
 
-require 'autoload.php';
+require ROOT . 'System/autoload.php';
 
 $router = new App\System\Router;
 $router->run();
