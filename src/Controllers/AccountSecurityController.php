d112158b81da4a265ec8d3a411569944355e9e28
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-07-15 20:03:31 +0700

    T4 - 10 - Controller - add functionality of processing data and form json response

--- src/Controllers/AccountSecurityController.php
+++ src/Controllers/AccountSecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\System\Controller;
 use App\Models\Users\User;
+use App\Controllers\SendEmail;
 
 class AccountSecurityController extends Controller
 {
@@ -29,11 +30,47 @@ class AccountSecurityController extends Controller
         if (!empty($_POST) && $this->user == null) {
             $data = json_decode($_POST['all'], true);
             $response = User::checkLoginEmail($data['data']);
-            echo json_encode($response);
+            if ($response != '') {
+                echo json_encode($response);
+            } else {
+                $newPassword = self::newPasswordGeneration();
+                $message = [
+                    'title' => 'Восстановление пароля на Sandbox',
+                    'body' => "
+                        <h2>Новое письмо</h2>
+                        <p>
+                            <b>Новый пароль:</b>$newPassword
+                        </p>
+                        <b>Рекомендуем после восстановление изменить пароль на свое усмотрение</b>
+                    ",
+                    'email' => [
+                        'email' => 'muravlevsvyatoslav@gmail.com',
+                        'name' => 'Техническая поддержка Sandbox'
+                    ]
+                ];
+                $status = SendEmail::passwordRecovery($data['data']['email'], $newPassword, $message);
+                echo json_encode($status);
+                if ($status== 'success') {
+                    $user = User::getUserByLogin($data['data']['login']);
+                    User::changePassword($user, $newPassword);
+                }
+            }
         } else {
             return false;
         }
         return true;
+    }
+
+    public static function newPasswordGeneration(): string
+    {
+        $comb = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $password = array();
+        $combLen = strlen($comb) - 1;
+        for ($i = 0; $i < 8; $i++) {
+            $n = rand(0, $combLen);
+            $password[] = $comb[$n];
+        }
+        return implode($password);
     }
 
     private static function pageListData(): array
