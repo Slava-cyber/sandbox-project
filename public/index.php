dde75c56378fb9382cd570f299bbcc8c2a34111e
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-07-19 17:41:46 +0700

    T4 - 7 - index - optimize the condition(if) block to enable error display

--- public/index.php
+++ public/index.php
@@ -2,12 +2,7 @@
 define('ROOT', dirname(__FILE__) . '/../src/');
 
 $config = require ROOT . '../settings/db.php';
-$errorStatus = false;
-if (isset($config['error'])) {
-    $errorStatus = $config['error'];
-}
-
-if ($errorStatus) {
+if (!empty($config['errorDisplay'])) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
