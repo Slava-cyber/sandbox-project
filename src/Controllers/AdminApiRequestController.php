cfff89666d4f07bdc0058766c07bafc1ef2a7761
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-08-09 19:56:13 +0700

    T6 - 5 - Controller, template - add functionality of sending email notification when some user or event has been deleted by admin

--- src/Controllers/AdminApiRequestController.php
+++ src/Controllers/AdminApiRequestController.php
@@ -8,6 +8,7 @@ use App\Models\Events\Requests;
 use App\Models\Validation\Validation as Validation;
 use App\System\Controller;
 use App\System\Model;
+use App\System\View;
 
 class AdminApiRequestController extends Controller
 {
@@ -74,6 +75,44 @@ class AdminApiRequestController extends Controller
         return true;
     }
 
+    public function actionUserChangeRole(): bool
+    {
+        if ($this->user instanceof User && $this->user->getRole() == 'administrator') {
+            $data = json_decode(file_get_contents('php://input', true));
+            $user = User::findOneByColumn('id', $data->id);
+            if ($user != null) {
+                $user->setRole($data->role);
+                $user->save();
+            }
+            $status = true;
+        } else {
+            $status = false;
+        }
+        echo json_encode(['status' => $status]);
+        return true;
+    }
+
+    public function actionEventGetDataById(): bool
+    {
+        $eventData = [];
+        if ($this->user instanceof User && $this->user->getRole() == 'administrator') {
+            $data = json_decode(file_get_contents('php://input', true));
+            $event = Event::findOneByColumn('id', $data->id);
+            if ($event != null) {
+                $status = true;
+                $eventData[0] = $event;
+                $result = self::formArrayOfArraysInsteadOfClassObjects($eventData);
+                $result[0]['author'] = $result[0]['author']->getLogin();
+                $eventData = $result[0];
+            } else {
+                $status = false;
+            }
+        } else {
+            $status = false;
+        }
+        echo json_encode(['status' => $status, 'event' => $eventData]);
+        return true;
+    }
 
     public function actionAllDataRelease(): bool
     {
@@ -99,6 +138,10 @@ class AdminApiRequestController extends Controller
             $entityClassName = self::entityClassNameArray()[$class];
             $object = $entityClassName::findOneByColumn('id', $id);
             if ($object != null) {
+                if (isset(self::arrayOfRemovalNotificationFormEmailFunctions()[$class])) {
+                    $functionName = self::arrayOfRemovalNotificationFormEmailFunctions()[$class];
+                    self::$functionName($this->user, $object);
+                }
                 $object->delete();
                 $status = 'success';
             } else {
@@ -110,18 +153,66 @@ class AdminApiRequestController extends Controller
         return true;
     }
 
+    private static function sendUserRemovalNotification(?User $admin, ?User $user): void
+    {
+        $email = $user->getEmail();
+        if ($email != null) {
+            $body = View::render(
+                'SendEmail/userRemovalNotification',
+                $admin,
+                [
+                    'login' => $user->getLogin(),
+                ]
+            );
+            $message = [
+                'title' => 'Ваш аккаунт на sandbox был удален',
+                'body' => $body,
+            ];
+            SendEmail::simpleMessageWithoutFiles($email, $message);
+        }
+    }
+
+    private static function sendEventRemovalNotification(?User $admin, ?Event $event): void
+    {
+        $email = $event->getAuthor()->getEmail();
+        if ($email != null) {
+            $body = View::render(
+                'SendEmail/eventRemovalNotification',
+                $admin,
+                [
+                    'title' => $event->getTitle(),
+                ]
+            );
+            $message = [
+                'title' => 'Ваш ивент на sandbox был удален',
+                'body' => $body,
+            ];
+            SendEmail::simpleMessageWithoutFiles($email, $message);
+        }
+    }
+
+    private static function arrayOfRemovalNotificationFormEmailFunctions(): ?array
+    {
+        return [
+            'user' => 'sendUserRemovalNotification',
+            'event' => 'sendEventRemovalNotification',
+        ];
+    }
+
     public function actionEventCreate(): bool
     {
         $response['status'] = true;
         if ($this->user instanceof User && $this->user->getRole() == 'administrator') {
             $data = json_decode(file_get_contents('php://input', true));
-            $data = json_decode(json_encode($data), true)['data'];
+            $id = $data->id;
+            $data = $data->data;
+            $data = json_decode(json_encode($data), true);
             $_POST = $data;
             $valid = new Validation($data, 'adminEvent');
             $response = $valid->validate();
             if ($response['status'] === true) {
                 $user = User::findOneByColumn('login', $_POST['author']);
-                $event = Event::create($_POST, $user, 'adminEvent');
+                $event = Event::create($_POST, $user, 'adminEvent', $id);
                 if ($event == null) {
                     $response['status'] = false;
                 }
